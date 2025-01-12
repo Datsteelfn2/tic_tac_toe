2a9b719c803c9b6100c77eef755b5156c72ae477
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-12 18:22:01 -0500

    Now the player or the bot cannot go any square that is already chosen

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ board=[[None,None,None],
        [None,None,None]]
 
 
-print(len(board))
+
 def display_board():
     for row in board:
         print(row)
@@ -15,13 +15,18 @@ def display_board():
 move=input("X or O:> ")
 
 while True:
-    bot_move_row=random.randint(-1,3)
-    bot_move_column=random.randint(-1,3)
+    bot_move_row=random.randint(0,2)
+    bot_move_column=random.randint(0,2)
     row=int(input("Which row would you like to place:> "))
     column=int(input("Which column:> "))
     
-    board[row][column]=move.upper()
-    board[bot_move_row][bot_move_column]="O"
+    
+    if board[row][column] is None:
+        board[row][column]=move.upper()
+        if board[bot_move_row][bot_move_column] is None:
+            board[bot_move_row][bot_move_column]="O"
+    elif board[row][column] is not None:
+        print("Spot taken")
     display_board()
     
     choice=input("exit:>")
@@ -29,7 +34,5 @@ while True:
         break
     else:
         continue
-    '''if len(board)==9:
-        break'''
 
 print("Youve exited the game")
