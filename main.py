d7abfa550ffed34362d9472ebf181fc65b2b2ee1
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-12 20:02:41 -0500

    updated bot logic to only move in empty squares, and player1 is the user by default and is X

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 import random
-
+counter=0
 
 
 board=[[None,None,None],
        [None,None,None],
        [None,None,None]]
 
-
-
+bot_move_row=0
+bot_move_column=0
 def display_board():
     for row in board:
         print(row)
 
-move=input("X or O:> ")
+print("WELCOME TO TIKTOK TOE, \033[31m you're PLAYER 1 and represent an  X\033[0m")
 
 while True:
-    bot_move_row=random.randint(0,2)
-    bot_move_column=random.randint(0,2)
+    
     row=int(input("Which row would you like to place:> "))
     column=int(input("Which column:> "))
     
-    
+    #players movement
     if board[row][column] is None:
         board[row][column]=move.upper()
-        if board[bot_move_row][bot_move_column] is None:
-            board[bot_move_row][bot_move_column]="O"
+        
     elif board[row][column] is not None:
         print("Spot taken")
+    #bot movement
+    while board[bot_move_row][bot_move_column] is not None:# if the spot is filled well keep finding new bot coordinates
+        bot_move_row=random.randint(0,2)
+        bot_move_column=random.randint(0,2)
+        
+    board[bot_move_row][bot_move_column]="O"
     display_board()
     
     choice=input("exit:>")
